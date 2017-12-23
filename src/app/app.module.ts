cdc78c993270c2640a7f21486933777e993fabbe
iliyaM <iliyuskeisar@gmail.com>
2017-12-23 17:08:37 +0200

    Removed routes from folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { appRoutes } from './routes/routes';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { appRoutes } from './routes';
 
 // FireStore AUTH
 import { CoreModule } from './core/core.module';
